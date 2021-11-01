5f2ccfaf27a78d793bd73db032d2a29b9c101dfc
Viktor Gubarevich <gubarew.v@gmail.com>
2021-11-01 10:34:32 +0300

    Create star-db application

---
+++ src/components/planet-details/planet-details.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './planet-details.css';
+
+export default class PlanetDetails extends Component {
+
+    render() {
+
+        return (
+            <div className="todo-app" >
+                Hello World
+            </div>
+        );
+    }
+}
